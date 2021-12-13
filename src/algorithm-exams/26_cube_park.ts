e985fbd2810077532a989645e77fe63a4cc68ea3
JoJo <yg@xkool.org>
2021-12-13 10:39:24 +0800

    26修改

--- src/algorithm-exams/26_cube_park.ts
+++ src/algorithm-exams/26_cube_park.ts
@@ -1,6 +1,13 @@
 type Coordinate = [number, number];
 
-// 快速解析坐标点 同时方便dict保存
+/*
+ * 这个代码花了很久 原因总结下：
+ * 1. 把广度遍历写成了深度优先（层序遍历变成了递归遍历，应该是按层递归，把下一层要用的状态保存然后给下层使用）
+ * 2. 循环状态的处理
+ * 3. 状态遍历的处理（避免重复访问，将一个有向图变成了一个树的访问，保存的访问状态没有处理好）
+ * 4. 广度优先的思考 首先是层序遍历（准备好下一层的数据，尾递归）；其次是标识node的访问状态，避免重复访问节点；确认访问状态（在每次push新节点前，判断是否已存在has，存在放弃，不存在push并记录状态）
+ */
+
 class RecordMemory {
   // 保存状态的dict 状态的key可以解析当前坐标点 和 空位置坐标点
   memory: { [key: string]: number };
@@ -75,39 +82,40 @@ function getMinCount(width: number, height: number) {
         result = steps;
         return;
       }
-
       directions.forEach(([dw, dh]) => {
         const nw = space[0] + dw;
         const nh = space[1] + dh;
-
         // 边界
         if (nw < 0 || nw >= width || nh < 0 || nh >= height) {
           return;
         }
-
         if (nw === target[0] && nh === target[1]) {
           // 交换位置
-          if (!pathMemory.has(space, target))
+          if (!pathMemory.has(space, target)) {
             newTargetList.push({ target: [...space], space: [...target] });
+            pathMemory.push([...space], [...target], steps + 1);
+          }
         } else {
-          if (!pathMemory.has(target, [nw, nh]))
+          if (!pathMemory.has(target, [nw, nh])) {
             newTargetList.push({ target: [...target], space: [nw, nh] });
+            pathMemory.push([...target], [nw, nh], steps + 1);
+          }
         }
       });
-
-      // 已经得到结果
-      if (result != null) {
-        return;
-      } else {
-        newTargetList.forEach(({ target, space }) => {
-          pathMemory.push([...target], [...space], steps);
-        });
-        moveRecursion(newTargetList, steps + 1);
-      }
     });
+
+    // 已经得到结果
+    if (result != null) {
+      return;
+    } else {
+      newTargetList.forEach(({ target, space }) => {
+        pathMemory.push([...target], [...space], steps + 1);
+      });
+      moveRecursion(newTargetList, steps + 1);
+    }
   };
   pathMemory.push([0, 0], [width - 1, height - 1], 0);
   moveRecursion([{ space: [width - 1, height - 1], target: [0, 0] }], 0);
   return result;
 }
-console.log(getMinCount(3, 3));
+console.log(getMinCount(10, 10));
