cd9735f8d5f2ef63c0a76e0aded453f6219433fc
Inouyasha <1348476929@qq.com>
2024-03-24 10:08:52 +0800

    15,16

---
+++ src/algorithm-exams/16_rope-square.ts
@@ -0,0 +1,10 @@
+// 绳子做成的长方形和两个正方形相同
+function getRopeSolution(len: number) {
+  // 正方形面积
+  const targetArea = (len / 4) * (len / 4);
+
+  const areaArr = [];
+  for(let i=1;i<len/4;i++){
+
+  }
+}
